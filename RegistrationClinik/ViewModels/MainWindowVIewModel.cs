dc71db28349d8d2f92f7b68565fa8035fbd617ba
DoFi24 <daniyarabdulhamidov@gmail.com>
2023-07-27 13:10:29 +0600

    wqwsa

--- RegistrationClinik/ViewModels/MainWindowVIewModel.cs
+++ RegistrationClinik/ViewModels/MainWindowVIewModel.cs
@@ -1,13 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
-using RegistrationClinik.Infras;
+﻿using RegistrationClinik.Infras;
 using RegistrationClinik.Models;
 using RegistrationClinik.Views;
-using System;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
-using System.Linq;
-using System.Windows;
-using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 
 namespace RegistrationClinik.ViewModels
---
+++ RegistrationClinik/ViewModels/RegBWindowViewModel.cs
@@ -0,0 +1,63 @@
+﻿using RegistrationClinik.Infras;
+using RegistrationClinik.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace RegistrationClinik.ViewModels
+{
+    public class RegBWindowViewModel : BaseViewModel
+    {
+        private BMainWindowViewModel model;
+        public RegBWindowViewModel(BMainWindowViewModel _model)
+        {
+            model = _model;
+            CreateCommand = new LambdaCommand(CreateCommandExcecute, CanCreateCommandExcecuted);
+            if (model.IsChange)
+                Item = model.SelectedClient;
+        }
+        public RegBWindowViewModel()
+        {
+
+        }
+
+        private DBTable item = new();
+        public DBTable Item
+        {
+            get { return item; }
+            set { Set(ref item, value); }
+        }
+        private string buttonName = "Сохранить";
+        public string ButtonName
+        {
+            get { return buttonName; }
+            set { buttonName = value; }
+        }
+        public ICommand CreateCommand { get; set; }
+        private bool CanCreateCommandExcecuted(object arg)
+        {
+            ButtonName = model.IsChange ? "Изменить" : "Сохранить";
+            return true;
+        }
+        private void CreateCommandExcecute(object obj)
+        {
+            using (ApplicationConnect db = new ApplicationConnect())
+            {
+                if (model.IsChange)
+                {
+                    db.DBTables.Add(Item);
+                }
+                else
+                {
+                    var result = db.DBTables.FirstOrDefault(s => s.Id == Item.Id);
+                    result = Item;
+                }
+                db.SaveChanges();
+                model.GetAllDate();
+            }
+        }
+    }
+}
